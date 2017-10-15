8b9dcb4132a0208bc027c51878656427c62a2155
pug3eye <pug3eye@gmail.com>
2017-10-16 02:05:12 +0700

    outsourcing Animations

---
+++ src/app/animations.ts
@@ -0,0 +1,47 @@
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+export const clickedStateTrigger = trigger('clickedState', [
+  state('default', style({
+    backgroundColor: 'orange',
+    width: '100px',
+    height: '100px'
+  })),
+  state('clicked', style({
+    backgroundColor: 'blue',
+    width: '300px',
+    height: '50px'
+  })),
+  state('mousedown', style({
+    backgroundColor: 'red',
+    border: '1px solid black',
+    width: '100px',
+    height: '100px'
+  })),
+  transition('default => clicked', animate('1s 500ms ease-in')),
+  // transition('clicked => default', animate('300ms ease-out')),
+  transition('clicked => default', animate(300)),
+  transition('mousedown <=> clicked', animate(300)),
+  // transition('clicked => mousedown', animate(300))
+]);
+
+export const numberEnteredStateTrigger =  trigger('numberEnteredState', [
+  state('unselected', style({
+    border: '1px solid black',
+    padding: '5px'
+  })),
+  state('selected', style({
+    border: '2px solid blue',
+    padding: '4px',
+    backgroundColor: 'lightblue'
+  })),
+  transition('unselected => selected', [
+    style({
+      border: '2px solid black',
+      padding: '4px'
+    }),
+    animate(300, style({
+      backgroundColor: 'red'
+    })),
+    animate(300)
+  ])
+]);
