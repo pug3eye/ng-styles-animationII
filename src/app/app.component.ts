60981fe4408689bb379120a4e7ddc1bdd073ce5a
pug3eye <pug3eye@gmail.com>
2017-10-16 01:00:25 +0700

    multiple transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'orange',
+        width: '100px',
+        height: '100px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'blue',
+        width: '300px',
+        height: '50px'
+      })),
+      transition('default => clicked', animate('1s 500ms ease-in')),
+      // transition('clicked => default', animate('300ms ease-out')),
+      transition('clicked => default', animate(300))
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+  clickInfo = 'default';
+
+  onClickSimple() {
+   this.clickInfo = 'clicked';
+   setTimeout(() => {
+     this.clickInfo = 'default';
+   }, 3000);
+  }
 }
